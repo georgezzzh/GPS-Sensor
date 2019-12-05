30644bff648f12592270c54cff895cdaad8b2bb5
George <usasne@163.com>
2019-12-05 13:01:18 +0800

    Update path.m

--- path.m
+++ path.m
@@ -1,32 +1,16 @@
 clear;
-m=csvread('C:\Users\George\Desktop\accelerate.csv',1,0);
-timeInterval=0.1;
+%数据格式设置为long
+format long g;
+%将csv数据读入到m中
+m=csvread('C:\Users\George\Desktop\gps_info.csv',1,0);
 ax=m(:,2);
 ay=m(:,3);
-t=0.1*ones(length(ax),1);
-vx=ones(length(ax),1);
-vx(1)=0;
-for i=2:length(ax)
-    vx(i)=vx(i-1)+ax(i)*timeInterval;
+time=m(:,1);
+%将时间戳转换为时间间隔
+for i=length(time):-1:2
+    time(i)=(time(i)-time(i-1))/1000;
 end
-vy=ones(length(ay),1);
-vy(1)=0;
-for i=2:length(ay)
-    vy(i)=vy(i-1)+ay(i)*timeInterval;
-end
-
-dx=ones(length(vx),1);
-dx(1)=0;
-dy=ones(length(vx),1);
-dy(1)=0;
-for i=2:length(vx)
-    dx(i)=dx(i-1)+vx(i)*timeInterval;
-end
-for i=2:length(vy)
-    dy(i)=dy(i-1)+vy(i)*timeInterval;
-end
-disp(dx);
-disp(dy)
-
-plot(dx,dy)
-axis([-10 30 -40 20])
+time(1)=0;
+%绘制出来
+plot(ay,ax);
+%xlim([104.0826 104.0855])
